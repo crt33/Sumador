bcd8a58f0ff6128c7f35e8c6007b357c846a48bb
Celia <Celia@Celia-Portatil>
2020-02-29 20:56:16 +0100

    Prueba1

--- Sumador/src/ASumar.java
+++ Sumador/src/ASumar.java
@@ -1,9 +1,34 @@
+/**
+ * 
+ * @author Celia
+ *
+ */
 
 public class ASumar {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private String valor_inicial;
+	
+	/**
+	 * 
+	 * @param string
+	 * 
+	 */
+	
+	public ASumar(String string) {
+		this.valor_inicial = string;
 	}
+	
+	/**
+	 * 
+	 * @return devuelve valor en el caso de que sea número de un dígito
+	 * 
+	 */
 
+    public String mostrar() {
+    	String numero = valor_inicial;
+    	if(numero.length() == 1) {
+    		return (numero + "=" + numero);
+    	}
+    	    return null;
+    }
 }
--- Sumador/test/TestASumar.java
+++ Sumador/test/TestASumar.java
@@ -2,11 +2,23 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/**
+ * 
+ * @author Celia
+ *
+ */
+
 public class TestASumar {
 
+	/**
+	 * prueba con número de sólo un dígito
+	 */
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testUnaCifra() {
+		ASumar sumi = new ASumar("7");
+		String resultado = sumi.mostrar();
+		assertEquals("7=7",resultado);
 	}
 
 }
